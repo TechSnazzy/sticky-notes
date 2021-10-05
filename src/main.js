6637f2622acf4913d4a33c65641b0255cee8cf86
Sean Morrison <sean1972@gmail.com>
2021-10-04 21:24:02 -0700

    Declare all functions that will be used in this app

---
+++ src/main.js
@@ -0,0 +1,46 @@
+/*
+Retrieve all the existing notes
+from the local storage of the client browser.
+*/
+function getNotes() {
+
+}
+
+/*
+This will take in an array of notes
+and will save the local notes in the
+storage of the clients browser.
+*/
+function saveNotes(notes) {
+
+}
+
+/*
+Build a new element to represent a note element.
+The element is an html element.
+*/
+function createNoteElement(id, content) {
+
+}
+
+/*
+Adding a new note not only to the HTML but
+also saving it to the local storage of the client browser.
+*/
+function addNote() {
+
+}
+
+/*
+This will update your note instead of adding a new one.
+*/
+function updateNote(id, newContent) {
+
+}
+
+/*
+Delete a note.
+*/
+function deleteNote(id, element) {
+
+}
