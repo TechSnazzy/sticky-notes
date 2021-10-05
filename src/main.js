2d56981c0b7345bb91598351a859572eba9d0136
Sean Morrison <sean1972@gmail.com>
2021-10-04 21:35:30 -0700

    Remove all the example textarea elements since they will be created with javascript instead

--- src/main.js
+++ src/main.js
@@ -1,50 +1,36 @@
-/*
-Declare some things.
-/*
+const notesContainer = document.getElementById('app');
 
 /*
 Retrieve all the existing notes
 from the local storage of the client browser.
 */
-function getNotes() {
-
-}
+function getNotes() {}
 
 /*
 This will take in an array of notes
 and will save the local notes in the
 storage of the clients browser.
 */
-function saveNotes(notes) {
-
-}
+function saveNotes(notes) {}
 
 /*
 Build a new element to represent a note element.
 The element is an html element.
 */
-function createNoteElement(id, content) {
-
-}
+function createNoteElement(id, content) {}
 
 /*
 Adding a new note not only to the HTML but
 also saving it to the local storage of the client browser.
 */
-function addNote() {
-
-}
+function addNote() {}
 
 /*
 This will update your note instead of adding a new one.
 */
-function updateNote(id, newContent) {
-
-}
+function updateNote(id, newContent) {}
 
 /*
 Delete a note.
 */
-function deleteNote(id, element) {
-
-}
+function deleteNote(id, element) {}
